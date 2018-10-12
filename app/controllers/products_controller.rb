e46170b77d7596a45171ca04f6b7c97ed043a5a3
paulabarreto <paulavilaca@gmail.com>
2018-10-12 21:43:54 +0000

    Logged in users can add reviews and delete their own

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,21 @@ class ProductsController < ApplicationController
     @review = @product.reviews.new
   end
 
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_review
+  #     @review = @product.reviews.find(params[:id])
+  #   end
+  #
+  #   def set_product
+  #     @product = Product.find(params[:product_id])
+  #   end
+
+    # # Never trust parameters from the scary internet, only allow the white list through.
+    # def dog_params
+    #   params.require(:dog)
+    #     .permit(:name, :colour, :date_of_birth)
+    #     .merge(owner: @owner)
+    # end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,32 @@
 class ReviewsController < ApplicationController
-
+  before_action :require_login
 
   def new
     @review = Review.new
   end
 
   def create
-    @review = Review.new(:product => @product)
-    @review.user_id = current_user
+    @product = Product.find(params[:product_id])
+    @review = Review.new(:product => @product, :description => params[:review][:description], :rating => params[:review][:rating])
+    @review.user = current_user
+    if @review.save
+      redirect_to @product
+    else
+      redirect_to @product, notice: "error"
+    end
+  end
+  def destroy
+    @review = Review.find_by(id: params[:id])
+    @review.destroy
+    redirect_to product_path(params[:product_id])
+  end
 
-    @review.save!
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
   end
 end
